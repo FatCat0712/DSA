711460f4232a67aafaa7d5758207a9374cd3923c
FatCat0712 <maisonhai456@gmail.com>
2024-02-10 12:16:28 +0700

    refactor ThapHaNoi

--- recursion/ThapHaNoi.java
+++ recursion/ThapHaNoi.java
@@ -5,20 +5,20 @@ import java.util.Scanner;
 public class ThapHaNoi {
     static char[] c = {'A','B','C'};
 
-    public static void thapHaNoi(int n,int i, int j,int k){
+    public static void thapHaNoi(int n,int nguon, int trunggian,int dich){
         if(n == 1){
-            System.out.println("Chuyen dia "+n+" tu cot "+c[i]+" sang cot "+c[j]);
+            System.out.println("Chuyen dia "+n+" tu cot "+c[nguon]+" sang cot "+c[dich]);
         }
         else{
-            thapHaNoi(n-1,i,k,j);
-            System.out.println("Chuyen dia "+n+" tu cot "+c[i]+" sang cot "+c[j]);
-            thapHaNoi(n-1,k,j,i);
+            thapHaNoi(n-1,nguon,dich,trunggian);
+            System.out.println("Chuyen dia "+n+" tu cot "+c[nguon]+" sang cot "+c[dich]);
+            thapHaNoi(n-1,trunggian,nguon,dich);
         }
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        thapHaNoi(n,0,2,1);
+        thapHaNoi(n,0,1,2);
     }
 }
